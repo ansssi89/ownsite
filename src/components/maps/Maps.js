e0815124b8f9a3f2b5d365839b615e00a75b310d
ansssi89 <ansssi@live.com>
2019-05-08 11:13:33 +0300

    maps marker

--- src/components/maps/Maps.js
+++ src/components/maps/Maps.js
@@ -1,23 +1,61 @@
 import React, {Component} from 'react';
-import {Map, GoogleApiWrapper} from 'google-maps-react';
+import {Map, GoogleApiWrapper, InfoWindow, Marker} from 'google-maps-react';
 
 const mapStyles = {
-    width: '100%',
-    height: '100%'
+    width: '80%',
+    height: '80%'
 };
 
 export class MapContainer extends Component {
+    state = {
+        showingInfoWindow: false,
+        activeMarker: {},
+        selectedPlace: {}
+    };
+
+    onMarkerClick = (props, marker, e) =>
+        this.setState({
+            selectedPlace: props,
+            activeMarker: marker,
+            showingInfoWindow: true
+        });
+
+        onClose = props => {
+            if(this.state.showingInfoWindow) {
+                this.setState({
+                    showingInfoWindow: false,
+                    activeMarker: null
+                });
+            }
+        };
+
     render() {
         return (
-            <Map 
+            <Map
                 google={this.props.google}
-                zoom={14}
+                zoom={17}
                 style={mapStyles}
                 initialCenter={{
                     lat: 61.0059208,
                     lng: 25.6395485
                 }}
-            />
+                >
+                <Marker 
+                    onClick={this.onMarkerClick}
+                    name={'Anssif'}
+                />
+                <InfoWindow
+                    marker={this.state.activeMarker}
+                    visible={this.state.showingInfoWindow}
+                    onClose={this.onClose}
+                >
+                    <div>
+                        <h4>{this.state.selectedPlace.name}</h4>
+                    </div>
+                </InfoWindow>
+            </Map>
+                
+            
         );
     }
 }
