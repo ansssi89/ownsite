17b14d4a128fca722f1f7e6f3b652379c4b71462
ansssi89 <ansssi@live.com>
2019-05-08 08:47:24 +0300

    contact

--- src/App.js
+++ src/App.js
@@ -2,11 +2,13 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 import Info from './components/info/Info';
+import Contact from './components/contact/Contact';
 
 function App() {
   return (
     <div className="App">
       <Info />
+      <Contact />
     </div>
   );
 }
---
+++ src/components/contact/Contact.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const contact = () => (
+    <div>
+        <h2>Infosivu</h2>
+    </div>
+);
+
+export default contact;
