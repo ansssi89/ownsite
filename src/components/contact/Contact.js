5c727e2e1c72174f8d7829686bcfa320bf3181e0
ansssi89 <ansssi@live.com>
2019-05-08 10:24:42 +0300

    contacts

--- src/components/contact/Contact.js
+++ src/components/contact/Contact.js
@@ -2,7 +2,11 @@ import React from 'react';
 
 const contact = () => (
     <div>
-        <h2>Contact page</h2>
+        <h2>Contact:</h2>
+        <p>mobile: +358 44 9879984</p>
+        <p>email: anssif7@gmail.com</p>
+        <p>https://github.com/ansssi89</p>
+        <p>https://www.linkedin.com/in/anssi-forsman-111496179/</p>
     </div>
 );
 
