f5a5aa7f0201ccaa5c363462758382ed7dcd41e4
ansssi89 <ansssi@live.com>
2019-05-08 08:40:43 +0300

    add info

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import Info from './components/info/Info';
 
 function App() {
   return (
     <div className="App">
-      
+      <Info />
     </div>
   );
 }
---
+++ src/components/info/Info.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const info = () => (
+    <div>
+        <h2>Infosivu</h2>
+    </div>
+);
+
+export default info;
